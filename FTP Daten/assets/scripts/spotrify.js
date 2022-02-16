a7398289e9b86682027953709f51c2f9f3c92f20
Chris Ro <christopher.rock@student.uni-luebeck.de>
2022-02-16 19:07:14 +0100

    spotrifyyyyeah

--- FTP Daten/assets/scripts/spotrify.js
+++ FTP Daten/assets/scripts/spotrify.js
@@ -1,5 +1,61 @@
-let navOpen = false;
+let navOpen = false, playState = false;
+let seekSlider = document.getElementById("audio-seek-slider");
+let playIcon = document.getElementById("playIcon");
+let audioBtn = document.getElementById("playBtn");
+const audio = document.querySelector('audio');
+const durationContainer = document.getElementById('duration');
+const currentContainer = document.getElementById('current-time');
 let menu = document.querySelector(".menu");
+
+audioBtn.addEventListener('click', () => {
+    if(!playState) {
+        audio.play();
+        playIcon.textContent = "❚❚";
+      } else {
+        audio.pause();
+        playIcon.textContent = "▶";
+      }
+    playState = !playState;
+});
+
+const setSliderMax = () => {
+    seekSlider.max = Math.floor(audio.duration);
+}
+
+audio.addEventListener('timeupdate', () => {
+    let time = Math.floor(audio.currentTime);
+    seekSlider.value = time;
+    currentContainer.textContent = calculateTime(audio.currentTime)
+
+    seekSlider.style.backgroundSize = (time - seekSlider.min) * 100 / (seekSlider.max - seekSlider.min) + '% 100%'
+  });
+
+seekSlider.addEventListener('change', () => {
+    audio.currentTime = seekSlider.value;
+    currentContainer.textContent = calculateTime(audio.currentTime)
+  });
+
+if (audio.readyState > 0) {
+    displayDuration();
+    setSliderMax();
+  } else {
+    audio.addEventListener('loadedmetadata', () => {
+      displayDuration();
+      setSliderMax();
+    });
+  }
+
+const displayDuration = () => {
+    durationContainer.textContent = calculateTime(audio.duration);
+}
+
+const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${minutes}:${returnedSeconds}`;
+}
+
 
   function openNav() {
     menu.classList.toggle("active");
