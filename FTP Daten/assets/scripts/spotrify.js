b1f41978a13d94b2a9a12cd981c2802de9574535
Chris Ro <christopher.rock@student.uni-luebeck.de>
2022-02-16 23:31:17 +0100

    spotteeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeefy

--- FTP Daten/assets/scripts/spotrify.js
+++ FTP Daten/assets/scripts/spotrify.js
@@ -2,10 +2,51 @@ let navOpen = false, playState = false;
 let seekSlider = document.getElementById("audio-seek-slider");
 let playIcon = document.getElementById("playIcon");
 let audioBtn = document.getElementById("playBtn");
+let trackTitle = document.getElementById("track-title");
+let albumImg = document.getElementById("album-img");
+let artistName = document.getElementById("artist-name");
 const audio = document.querySelector('audio');
 const durationContainer = document.getElementById('duration');
 const currentContainer = document.getElementById('current-time');
 let menu = document.querySelector(".menu");
+let currentTrack = 0;
+let tracksArray = [{trackName: "Cosmetic Geometry", artist: "atherrasool1111", coverSrc: "tri_cosmetic.png", trackSrc: "cosmeticgeometry.mp3"},
+{trackName: "Lorem Ipsum", artist: "Jona K", coverSrc: "tri_jona.png", trackSrc: "cosmeticgeometry.mp3"},
+{trackName: "Spirit Blossom", artist: "RomanBelov", coverSrc: "tri_spirit.png", trackSrc: "spiritblossom.mp3"}]
+
+updateTrack(0);
+
+function updateTrack(dir){
+    switch (dir) {
+        case 1:
+            if(currentTrack === tracksArray.length-1){
+                currentTrack = 0;
+            }else{
+                currentTrack += 1;
+            }
+            break;
+        case -1:
+            if(currentTrack === 0){
+                currentTrack = tracksArray.length -1;
+            }else{
+                currentTrack -= 1;
+            }
+            break;
+        default:
+            break;
+    }
+
+    console.log(currentTrack)
+    trackTitle.textContent = tracksArray[currentTrack].trackName;
+    artistName.textContent = tracksArray[currentTrack].artist;
+    albumImg.src = `assets/covers/${tracksArray[currentTrack].coverSrc}`;
+    audio.src=`assets/tracks/${tracksArray[currentTrack].trackSrc}`;
+
+    if(dir !== 0 && playState){
+        audio.play();
+    }
+}
+
 
 audioBtn.addEventListener('click', () => {
     if(!playState) {
